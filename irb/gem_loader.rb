bb064827bd7aa0ebb8d22775ecc96bd0c105a9f3
Burin Choomnuan <agilecoders@gmail.com>
2013-06-24 11:16:34 +1000

    Allow the user with rbenv to load the config if available, use rvm otherwise

--- irb/gem_loader.rb
+++ irb/gem_loader.rb
@@ -2,8 +2,19 @@ module IRB
   module GemLoader
     def self.setup
       if defined?(Bundler)
+        # if using RBENV, use the next two lines
+        gempath = `gem env gempath`
+        global_gemset = nil
+
+        if gempath && (/rbenv/ =~ gempath)
+          # for RBENV user
+          global_gemset = gempath.split(":").first
+        else
+          # for RVM user
+          global_gemset = ( ENV["GEM_PATH"] || `rvm $(rvm current) do gem env path`.chop ).split(':').grep(/ruby.*@global/).first
+        end
+
         # We make everything in the global gemset available, bypassing Bundler
-        global_gemset = ( ENV["GEM_PATH"] || `rvm $(rvm current) do gem env path`.chop ).split(':').grep(/ruby.*@global/).first
         Dir["#{global_gemset}/gems/*"].each { |path| $LOAD_PATH << "#{path}/lib" } if global_gemset
       end
     end
@@ -19,4 +30,5 @@ module IRB
       false
     end
   end
+
 end
